333e3acc3adf25029829c80b2b2c04e35f25cf8e
김건희 <gimgeonhui@gimgeonhuiui-Macmini.local>
2025-07-01 00:16:47 +0900

    Day 1: Average program

---
+++ week1/day01/average.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10, y = 20, z = 30;
+    float average = (x + y + z) / 3.0;
+    cout << "Average: " << average << endl;
+    return 0;
+}
